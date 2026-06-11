58b12502b532881d7952c21bbab3b7f26eb57cf2
Ida Touray <46387179+ITouray@users.noreply.github.com>
2026-06-11 15:02:12 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h3>The Ultimate Plant Parent!</h3>
+      <p>How good of a plant parent are you? Test all of your planty knowledge here!</p>
+      <p>Number of cards:</p>
+
+      <div className="card-container">
+        <div className="card">
+          {/* <h4>Easy</h4> */}
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
